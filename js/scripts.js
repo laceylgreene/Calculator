95dad6020579c1d5447fe02b7bb954f07d39cb2e
Emily Sershon and Lacey Greene <lacey@>
2017-02-01 19:51:52 -0800

    add bmi prompts and result functions with 0 fixed decimal places

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,16 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
 alert(result);
+
+
+//BMI: Multiply the weight in pounds by 0.45 (the metric conversion factor)
+
+var userWeight = parseInt(prompt ("How much do you weigh in lbs?"));
+var userHeight = parseInt(prompt ("How tall are you in inches?"));
+var userBMI = function (height, weight){return ((weight*703)/(height*height))}
+
+alert ("Your BMI is " + userBMI(userHeight, userWeight).toFixed() + ".");
+
+
+
+//F to C: The temperature T in degrees Fahrenheit (°F) is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32:
